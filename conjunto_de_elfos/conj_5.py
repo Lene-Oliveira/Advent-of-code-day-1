9fa096de6cf1d6e430bb53d765d8ddaa2fcb3f57
Francilene Oliveira <Francilene_Oliveira@proton.me>
2022-12-04 20:38:08 +0000

    declaration of int in the lists

--- conjunto_de_elfos/conj_5.py
+++ conjunto_de_elfos/conj_5.py
@@ -1,29 +1,44 @@
 LelfoA = [ "17979", "3250", "19984"]
+int_LelfoA = map(int, LelfoA)
 
 LelfoB = [ "5650", "5756", "3307", "10549", "6717", "9579", "2849", "6885"]
+int_LelfoB = map(int, LelfoB)
 
 LelfoC = [ "2609", "2347", "4692", "5000", "5068", "5148", "4337", "4316", "2654", "2221", "1822"]
+int_LelfoC = map(int, LelfoC)
 
 LelfoD = [ "13157"]
+int_LelfoD = map(int, LelfoD)
 
 LelfoE = [ "10140", "5075", "10446", "2946", "5902", "5856", "8096", "8762"]
+int_LelfoE = map(int, LelfoE)
 
 LelfoF = [ "16951", "9118", "17962"]
+int_LelfoF = map(int, LelfoF)
 
 LelfoG = [ "7524", "4389", "3716", "3580", "5930", "1197", "3233", "6070", "4612", "2477", "6792"]
+int_LelfoG = map(int, LelfoG)
 
 LelfoH = [ "6403", "11307", "10753", "9665", "4919", "6485", "3189"]
+int_LelfoH = map(int, LelfoH)
 
 LelfoI = [ "8790", "10424", "8816", "3840", "6236", "5529", "8946", "3864"]
+int_LelfoI = map(int, LelfoI)
 
 LelfoJ = [ "1370", "4810", "3752", "1664", "1247", "3227", "4605", "4906", "5436", "4534", "4722", "4104", "5629", "4962", "3203"]
+int_LelfoJ = map(int, LelfoJ)
 
 LelfoK = [ "2120", "4830", "2859", "2828", "3020", "5260", "5795", "3286", "5247", "6126", "4156", "3624", "2730"]
+int_LelfoK = map(int, LelfoK)
 
 LelfoL = ["58973"]
+int_LelfoL = map(int, LelfoL)
 
 LelfoM = [ "18871", "8511"]
+int_LelfoM = map(int, LelfoM)
 
 LelfoN = [ "13088", "1608", "12123", "5162", "9357", "2409"]
+int_LelfoN = map(int, LelfoN)
 
-ElfoMusk = [ "3194", "2605", "8440"]
+Lelforiano = [ "3194", "2605", "8440"]
+int_Lelforiano = map(int, Lelforiano)
