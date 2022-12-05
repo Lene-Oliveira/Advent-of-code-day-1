2d97be2b4eeb9987270f06f0045af9fbd028851b
Francilene Oliveira <Francilene_Oliveira@proton.me>
2022-12-05 15:17:30 +0000

    basic comparision between the elvs calories

--- testes.py
+++ testes.py
@@ -4,7 +4,36 @@ import variaveis as varss
 varss.tsomaconj1 = varss.asoma1 + varss.asoma2 + varss.asoma3 + varss.asoma4 
 varss.tsomaconj2 = varss.bsoma1 + varss.bsoma2 + varss.bsoma3 + varss.bsoma4
 
-# testing if they can becompared
+# printing each elv calories
+print("Each elvs calories are: \n", 
+varss.asoma1, 
+"\n", varss.asoma2, 
+"\n", varss.asoma3, 
+"\n", varss.asoma4, 
+"\n", varss.asoma5, 
+"\n", varss.asoma6, 
+"\n", varss.asoma7,
+"\n", varss.asoma8,
+"\n", varss.asoma9,
+"\n", varss.asoma10,
+"\n", varss.asoma11,
+"\n", varss.asoma12,
+"\n", varss.asoma13,
+"\n", varss.asoma14,
+"\n", varss.asoma15,
+"\n", varss.asoma16,
+"\n", varss.asoma17,
+"\n", varss.asoma18,
+"\n", varss.asoma19,
+"\n", varss.asoma20,
+"\n", varss.asoma21,
+"\n", varss.asoma22,
+"\n", varss.asoma23,
+"\n", varss.asoma24,
+"\n", varss.asoma25
+ 
+)
+# defining which has more calories
 
 max1 = max(
     varss.asoma1,
@@ -31,7 +60,10 @@ max1 = max(
     varss.asoma22,
     varss.asoma23,
     varss.asoma24,
-    varss.asoma25,
-    ) 
-print(max1)
+    varss.asoma25
+
+) 
+
+print("the elv with the most calories is: ", max1)
+
 
